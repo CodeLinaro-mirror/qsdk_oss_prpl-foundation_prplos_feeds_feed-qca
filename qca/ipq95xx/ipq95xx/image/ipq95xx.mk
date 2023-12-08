0a8a9d5594a9d271ffd788a1b3d63fe58c7b205e
Marvin Lu <marvin.lu@wnc.com.tw>
2023-12-08 16:31:02 +0800

    ipq95xx: add support for WNC Freedom board
    Add the necessary packages to bring up Freedom.
    
    Signed-off-by: Marvin Lu <marvin.lu@wnc.com.tw>

---
+++ qca/ipq95xx/ipq95xx/image/ipq95xx.mk
@@ -0,0 +1,22 @@
+KERNEL_LOADADDR := 0x42080000
+
+define Device/qcom_al02-c4
+  DEVICE_TITLE := Qualcomm AP-AL02-C4
+  DEVICE_DTS := ipq9574-al02-c4
+  DEVICE_DTS_CONFIG := config@al02-c4
+  SUPPORTED_DEVICES := qcom,ipq9574-ap-al02-c4
+# DEVICE_PACKAGES := ath11k-wifi-tplink-ex227
+  IMAGES := sysupgrade.tar nand-factory.bin nand-factory.ubi
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/nand-factory.bin := append-ubi | qsdk-ipq-factory-nand
+  IMAGE/nand-factory.ubi := append-ubi
+endef
+TARGET_DEVICES += qcom_al02-c4
+
+define Device/prpl_freedom
+  DEVICE_TITLE := prpl Freedom
+  DEVICE_DTS := ipq9574-freedom
+  DEVICE_DTS_CONFIG := config@al02-c4
+  SUPPORTED_DEVICES := prpl,freedom
+endef
+TARGET_DEVICES += prpl_freedom
