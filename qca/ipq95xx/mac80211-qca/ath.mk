643817a4d4b51cef9fa76fbd200ec05846fce1a5
Marvin Lu <marvin.lu@wnc.com.tw>
2024-05-15 10:02:39 +0800

    mac80211-qca: disable ath12k cold_boot_cal
    This is to significantly reduce the loading time of ath12k driver and
    then speed up the linux booting.
    
    Signed-off-by: Marvin Lu <marvin.lu@wnc.com.tw>

--- qca/ipq95xx/mac80211-qca/ath.mk
+++ qca/ipq95xx/mac80211-qca/ath.mk
@@ -345,7 +345,7 @@ define KernelPackage/ath12k-qca
   DEPENDS+= +kmod-ath-qca +@DRIVER_11N_SUPPORT +@DRIVER_11W_SUPPORT +@DRIVER_11AC_SUPPORT +@DRIVER_11AX_SUPPORT
   FILES:=$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath12k/ath12k.ko
   AUTOLOAD:=$(call AutoProbe,ath12k)
-  MODPARAMS.ath12k:=frame_mode=1
+  MODPARAMS.ath12k:=frame_mode=1 cold_boot_cal=0
 endef
 
 define KernelPackage/ath12k-qca/description
