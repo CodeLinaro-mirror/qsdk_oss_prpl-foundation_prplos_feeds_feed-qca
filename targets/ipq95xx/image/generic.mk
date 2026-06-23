ede77450965183683207c4072a62af9c34ecb5e9
Marvin Lu <marvin.lu@wnc.com.tw>
2026-06-23 14:38:38 +0800

    ipq95xx: add image build for freedom model
    Add Prpl Freedom model support to ipq95xx target.
    
    References: PCF-2660
    
    Signed-off-by: Marvin Lu <marvin.lu@wnc.com.tw>

--- targets/ipq95xx/image/generic.mk
+++ targets/ipq95xx/image/generic.mk
@@ -75,15 +75,15 @@ define Device/qcom_rdp476
 endef
 TARGET_DEVICES += qcom_rdp476
 
-#define Device/prpl_freedom
-#	$(call Device/FitImage)
-#	$(call Device/EmmcImage)
-#	DEVICE_VENDOR := Prpl
-#	DEVICE_MODEL := Freedom
-#	DEVICE_DTS := ipq9574-freedom
-#	DEVICE_DTS_CONFIG := config@al02-c4
-#	SOC := ipq9574
-#	DEVICE_PACKAGES += ath12k-firmware-qcn92xx ath12k-wifi-qcom-qcn92xx kmod-ath12k \
-#		mkf2fs f2fsck kmod-fs-f2fs
-#endef
-#TARGET_DEVICES += prpl_freedom
+define Device/prpl_freedom
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := Prpl
+	DEVICE_MODEL := Freedom
+	DEVICE_DTS := ipq9574-freedom
+	DEVICE_DTS_CONFIG := config@al02-c4
+	SOC := ipq9574
+	DEVICE_PACKAGES += ath12k-firmware-qcn92xx ath12k-wifi-qcom-qcn92xx kmod-ath12k \
+		mkf2fs f2fsck kmod-fs-f2fs
+endef
+TARGET_DEVICES += prpl_freedom
